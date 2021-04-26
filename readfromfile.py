71ca96924a7a38a6e6681243ea0c2ab98e39c92f
fanalis93 <jaki.fayek@live.iium.edu.my>
2021-04-26 19:17:09 +0800

    Update readfromfile.py

--- readfromfile.py
+++ readfromfile.py
@@ -1,5 +1,5 @@
 def read_integers(filename):
     with open(filename) as f:
         return map(int, f)
-res = read_integers('F:/Programming Problems/sensordata.txt')
+res = read_integers('sensordata.txt')
 print(list(res))
